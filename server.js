c239e00da98470f7377c6ae216d1cd9ff8a00e15
obay <obaymardini@gmail.com>
2017-05-02 23:54:13 +0200

    organization and owner endpoints to the server

--- server.js
+++ server.js
@@ -19,36 +19,95 @@ var repoSchema = new Schema({
 
 var Repos = mongoose.model('Repos', repoSchema);
 
-
-
 app.use(bodyParser.json());
-console.log(__dirname + '\\' + 'src')
-app.use(express.static(__dirname + '\\' + 'src'));
+app.use(express.static(__dirname  + '/src'));
 
 app.get('/savedRepos/:username', function(req, res) {
   Repos.find({username: req.params.username}).exec(function(err, repos) {
     if(err) res.send(404, 'Error: ' + err);
-    res.JSON(users)
+    res.json(repos)
   });
 })
 
-app.post('/saveRepo/:repo', function(req, res) {
+app.post('/saveRepo/:username', function(req, res) {
+  console.log('saveRepo', req.params.username, req.body.name)
+  var repo = req.body;
   var newRepo = new Repos({
-    name: req.repo.name,
-    username: req.repo.username,
-    url: req.repo.url
+    name: repo.name,
+    username: req.params.username,
+    url: repo.url
   });
 
   newRepo.save(function(err) {
     if(err) {
       console.log(err);
+      res.send(404, 'Error:', err)                                                                                                                                                                                                                                                                  
     } else {
       console.log('repo saved successfully')
+      res.send(201,repo)
     }
 
   });
 });
 app.get('/getRepos/:userName', function(req, res) {
+  var options = {
+   uri: 'https://api.github.com/users/' + req.params.userName + '?access_token=' + passwords.API_KEY,
+   method: 'GET',
+   headers: {
+       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+       'Accept': 'application/json',
+       'User-agent': 'obay-mardini'
+   }
+  };
+  console.log(req.params.userName)
+  if(req.params.userName === 'obay-mardini') {
+    res.redirect('/owner');
+  } else {
+    console.log('here')
+    request(options, function(err, response, body) {
+      if(err) console.log(err);
+      console.log(JSON.parse(body).type)
+      if(JSON.parse(body).type === 'Organization') {
+        res.redirect(301, '/orgs/' + req.params.userName)
+      } else if(JSON.parse(body).type === 'User') {
+        res.redirect('/users/' + req.params.userName)
+      } 
+    });
+  }
+});
+
+app.get('/owner', function(req, res) {
+  var options = {
+   uri: 'https://api.github.com/user/repos?access_token=' + passwords.API_KEY,
+   method: 'GET',
+   headers: {
+       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+       'Accept': 'application/json',
+       'User-agent': 'obay-mardini'
+   }
+  };
+  request(options, function(err, response, body) {
+    if(err) res.status(401).json(err);
+    if(body) {res.status(200).json(body)}
+  })
+})
+app.get('/orgs/:userName', function(req, res) {
+  var options = {
+   uri: 'https://api.github.com/orgs/' + req.params.userName + '/repos?access_token=' + passwords.API_KEY,
+   method: 'GET',
+   headers: {
+       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+       'Accept': 'application/json',
+       'User-agent': 'obay-mardini'
+   }
+  };
+  request(options, function(err, response, body) {
+    if(err) res.status(404).end(err);
+    if(body) res.status(200).json(body);
+  })
+})
+
+app.get('/users/:userName', function(req, res) {
   var options = {
    uri: 'https://api.github.com/users/' + req.params.userName + '/repos?access_token=' + passwords.API_KEY,
    method: 'GET',
@@ -59,10 +118,10 @@ app.get('/getRepos/:userName', function(req, res) {
    }
   };
   request(options, function(err, response, body) {
-    if(err) console.log(err);
-    if(body) res.json(body)
-  });
-});
+    if(err) console.log('ERROR ', err);
+    if(body) res.status(200).json(body);
+  })
+})
 
 app.get('/forkRepo/:repoName/:userName', function(req, res) {
   var options = {
